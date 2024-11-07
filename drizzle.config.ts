4a938946dbc991aa9c16a7b0327decab9de3fedb
LovelessCodes <hello@loveless.codes>
2024-11-07 11:40:37 +0100

    fix: allow turso stuffs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'drizzle-kit';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
+const authToken = process.env.TURSO_DB_AUTH_TOKEN;
+
 export default defineConfig({
   schema: './src/lib/server/db/auth-schema.ts',
 
   dbCredentials: {
-    url: process.env.DATABASE_URL
+    url: process.env.DATABASE_URL,
+    authToken,
   },
 
   verbose: true,
   strict: true,
-  dialect: 'sqlite'
+  dialect: authToken ? 'turso' : 'sqlite'
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,5 +3,5 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as authSchema from "./auth-schema";
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ url: env.DATABASE_URL, authToken: env.TURSO_DB_AUTH_TOKEN });
 export const db = drizzle(client, { schema: authSchema });
